18cae5ae795e6b2981acb751e7ffae41c6f0f59e
fin del kind <fin+github@xbhd.org>
2010-10-22 01:34:07 +0200

    test mode for b.py

--- raw/b.py
+++ raw/b.py
@@ -3,11 +3,21 @@ import sys
 
 
 import lib.generate
+import SimpleHTTPServer, BaseHTTPServer
 
 if __name__=='__main__':
     if len(sys.argv)<2:
         print 'needs option: generate'
     if sys.argv[1]=='generate':
         lib.generate.generate(settings)
-    if sys.argv[1]=='post':
+    elif sys.argv[1]=='post':
         lib.generate.post(settings)
+    elif sys.argv[1]=='test':
+        lib.generate.generate(settings)
+        print 'launching on http://localhost:8889/'
+        try:
+            BaseHTTPServer.HTTPServer(('localhost',8889,),SimpleHTTPServer.SimpleHTTPRequestHandler).serve_forever()
+        except KeyboardInterrupt:
+            print '^C received, bye bye!'
+    else:
+        print 'unknown command'
